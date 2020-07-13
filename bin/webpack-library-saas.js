d41a5921b9bc2c2447ff5bd2d735b4916eb2a3f8
lei.wang <lei.wang@gongkong.com>
2020-07-13 20:32:35 +0800

    update webpack-library-saas.js

--- bin/webpack-library-saas.js
+++ bin/webpack-library-saas.js
@@ -6,50 +6,24 @@ process.on('unhandledRejection', err => {
   throw err;
 });
 
-const spawn = require('cross-spawn');
+const webpack = require('webpack');
 const args = process.argv.slice(2);
 
-const scriptIndex = args.findIndex(
-  x => x === 'build' || x === 'eject' || x === 'start' || x === 'test'
-);
-const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
-const nodeArgs = scriptIndex > 0 ? args.slice(0, scriptIndex) : [];
+if (args[0] === 'build') {
+  const prodConfig = require('../../../node_modules/webpack-library-saas/library/webpack.prod');
 
-console.log('process', process.argv);
-console.log('spawn', spawn);
-console.log('args', args);
-console.log('scriptIndex', scriptIndex);
-console.log('nodeArgs', nodeArgs);
-
-if (['build', 'eject', 'start', 'test'].includes(script)) {
-  const result = spawn.sync(
-    process.execPath,
-    nodeArgs
-      .concat(require.resolve('../scripts/' + script))
-      .concat(args.slice(scriptIndex + 1)),
-    { stdio: 'inherit' }
-  );
-  if (result.signal) {
-    if (result.signal === 'SIGKILL') {
-      console.log(
-        'The build failed because the process exited too early. ' +
-          'This probably means the system ran out of memory or someone called ' +
-          '`kill -9` on the process.'
-      );
-    } else if (result.signal === 'SIGTERM') {
-      console.log(
-        'The build failed because the process exited too early. ' +
-          'Someone might have called `kill` or `killall`, or the system could ' +
-          'be shutting down.'
-      );
+  webpack(prodConfig, (err, stats) => {
+    if (err) {
+      console.error(err);
+      process.exit(2);
     }
-    process.exit(1);
-  }
-  process.exit(result.status);
-} else {
-  console.log('Unknown script "' + script + '".');
-  console.log('Perhaps you need to update react-scripts?');
-  console.log(
-    'See: https://facebook.github.io/create-react-app/docs/updating-to-new-releases'
-  );
+
+    console.log(stats.toString({
+      colors: true,
+      modules: false,
+      children: false
+    }));
+  });
+} else if (args[0] === 'start') {
+  
 }
