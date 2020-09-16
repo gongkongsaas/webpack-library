95b85a694ea435d9d149f4c8eea07188d85cad59
lei.wang <lei.wang@gongkong.com>
2020-09-16 10:00:35 +0800

    update

--- library/dev.server.js
+++ library/dev.server.js
@@ -2,7 +2,6 @@ const open = require('open');
 const path = require('path');
 const webpack = require('webpack');
 const express = require('express');
-const cookieParser = require('cookie-parser');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
@@ -19,8 +18,6 @@ const middleware = webpackDevMiddleware(compiler, {
   publicPath: devConfig.output.publicPath,
 });
 
-app.use(cookieParser());
-
 app.use('/api', createProxyMiddleware({
   target,
   changeOrigin: true,
@@ -32,9 +29,9 @@ app.use(webpackHotMiddleware(compiler, {
   log: console.log,  // eslint-disable-line
 }));
 
-app.get(`/${appName}/:pageName/:item`, (req, res) => {
+app.get(`/${appName}/:version/:pageName/:item`, (req, res) => {
   let result = '';
-  const htmlPath = path.join(`${devConfig.output.path}/${req.params.pageName}/index.html`);
+  const htmlPath = path.join(`${devConfig.output.path}/${req.params.version}/${req.params.pageName}/index.html`);
 
   res.cookie('traceId', '12499d63d526a23d927f6d026389d46b');
   res.cookie('ticket', 'SHP_2_11094_USER-SESSION_2F3ECAE3BEC6EA5A3F19D7482E509D7D');
@@ -53,6 +50,6 @@ app.get(`/${appName}/:pageName/:item`, (req, res) => {
 // process.env.PORT
 app.listen(port, (err) => {
   if (!err) {
-    open(`http://localhost:${port}/${appName}/index`);
+    open(`http://localhost:${port}/${appName}/v5/index`);
   }
 });
