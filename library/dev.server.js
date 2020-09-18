5676af88c91a210076a9c15a38727691c91a6684
lei.wang <lei.wang@gongkong.com>
2020-09-18 17:55:56 +0800

    update

--- library/dev.server.js
+++ library/dev.server.js
@@ -33,9 +33,15 @@ app.get(`/${appName}/:version/:pageName/:item`, (req, res) => {
   let result = '';
   const htmlPath = path.join(`${devConfig.output.path}/${req.params.version}/${req.params.pageName}/index.html`);
 
-  res.cookie('traceId', '12499d63d526a23d927f6d026389d46b');
-  res.cookie('ticket', 'SHP_2_11094_USER-SESSION_2F3ECAE3BEC6EA5A3F19D7482E509D7D');
-  res.cookie('JSESSIONID', 'node01d5vyn8h0kc1w1uz05xg4c5qof11.node0');
+  if (target.indexOf('gongkongsaas') > 0) {
+    res.cookie('traceId', '6fec0648ab9dffb336f56c89ef7a4f25');
+    res.cookie('ticket', 'SHP_9_1_USER-SESSION_C1CC2452C9F76302C883922FE5516D73');
+    res.cookie('JSESSIONID', 'node0z3yf2z9gsfbbb5phiic8c8dj0.node0');
+  } else {
+    res.cookie('traceId', '12499d63d526a23d927f6d026389d46b');
+    res.cookie('ticket', 'SHP_2_11094_USER-SESSION_2F3ECAE3BEC6EA5A3F19D7482E509D7D');
+    res.cookie('JSESSIONID', 'node01d5vyn8h0kc1w1uz05xg4c5qof11.node0');
+  }
 
   try {
     result = middleware.fileSystem.readFileSync(htmlPath);
